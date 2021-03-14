09e33b0bf4288e0e450ea2987cb11d9a2650e905
Luiz Marcelo Schmitt <luizmarceloschmitt@gmail.com>
2021-03-13 21:52:40 -0300

    Adicionado pasta src com os diretórios básicos do projeto

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import {View} from 'react-native';
+import { Text, View } from 'react-native';
 
 const App: React.FC = () => (
-  <View style={{flex: 1, backgroundColor: '#312e38'}} />
+  <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+    <Text>Hello World!</Text>
+  </View>
 );
 
 export default App;
